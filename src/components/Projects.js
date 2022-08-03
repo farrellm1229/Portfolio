1f490a37953cbd871d7174575468aaf0d7389cc2
Matt Farrell <matthew.farrell2@marist.edu>
2022-08-02 21:46:16 -0400

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -143,11 +143,10 @@ export const Projects = () => {
                       <br></br><br></br>My domination continued to reign for two years, with multiple devices running my programs 24/7, until the developers of the game, "EA Sports", finally figured out how to remove this ability for "bots" to function. 
                       
                     </p>
-                    <span id="wrapper">
+                    
                     <video width="750" height="500" controls >
                     <source src={sonic5} type="video/mp4"/>
                   </video>
-                    </span>
                       
                     </Tab.Pane>
                     
