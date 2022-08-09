631f50b1f90cfe775fb091ef217d4237b1986333
Matt Farrell <matthew.farrell2@marist.edu>
2022-08-09 14:35:36 -0400

    navbar pic update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon2 from '../assets/img/outlook.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/matt-farrell-b70aa0205/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/matt-farrell-b70aa0205/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.instagram.com/matt_farrell_/" target="_blank"><img src={navIcon3} alt="" /></a>
                 <a href="mailto:matt.farrell1@outlook.com"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/matt_farrell_/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
