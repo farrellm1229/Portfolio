67a0c19eaeb7d57cb3ea87ec0cefcf9de6b45c6b
Matt Farrell <matthew.farrell2@marist.edu>
2022-08-10 13:23:07 -0400

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -54,9 +54,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
+                <a href={resume} target="_blank"><img src={navIcon4} alt="" /></a>
                 <a href="https://www.linkedin.com/in/matt-farrell-b70aa0205/" target="_blank"><img src={navIcon1} alt="" /></a>
                 <a href="https://www.instagram.com/matt_farrell_/" target="_blank"><img src={navIcon3} alt="" /></a>
-                <a href={resume} target="_blank"><img src={navIcon4} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
